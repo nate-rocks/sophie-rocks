117becb354258835ee31221ff0a65ea016c57daa
nate-rocks <nate.wessel@gmail.com>
2020-05-24 21:40:56 -0600

    Added cat collision and basic dialog baoc

--- GAME_REDO.py
+++ GAME_REDO.py
@@ -5,13 +5,31 @@ from pathlib import Path
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
+
+class DialogBox():
+    def __init__(self, text, sprite):
+        self.text = text
+        self.x = sprite.rect.x + sprite.width
+        self.y = sprite.rect.y
+
+    def redraw(self, window):
+        # If game isn't over, draw this stuff.
+        font = pygame.font.Font(None, 18)
+        text = font.render(self.text, True, WHITE)
+        text_rect = text.get_rect()
+
+        window.blit(text, [self.x, self.y])
+
+
 class Cat(pygame.sprite.Sprite):
     def __init__(self, init_x, init_y):
         pygame.sprite.Sprite.__init__(self)
         self.x = init_x
         self.y = init_y
+        self.prev_x = 0
+        self.prev_y = 0
         self.step_size = 5
-
+        self.message = None
         self.load_images()
         self.stand_still()
 
@@ -24,6 +42,7 @@ class Cat(pygame.sprite.Sprite):
         self.right_cat = pygame.image.load(r"{}\\{}".format(image_dir, 'right_still.png'))
 
         self.rect = pygame.Rect((self.x, self.y), self.front_cat.get_rect().size)
+        self.width, self.height = self.front_cat.get_rect().size
 
     def go_up(self):
         self.up = True
@@ -59,8 +78,14 @@ class Cat(pygame.sprite.Sprite):
         self.left = False
         self.down = False
 
-    def redraw_cat(self, window):
+    def undo_move(self):
+        self.x -= self.prev_x 
+        self.y -= self.prev_y
+
+    def redraw(self, window):
         self.rect.move_ip(self.x, self.y)
+        self.prex_x = self.x
+        self.prev_y = self.y
         if self.left:
             window.blit(self.left_cat, (self.rect.x, self.rect.y))
         elif self.right:
@@ -71,8 +96,14 @@ class Cat(pygame.sprite.Sprite):
             window.blit(self.back_cat, (self.rect.x, self.rect.y))
         else:
             window.blit(self.front_cat, (self.rect.x, self.rect.y))
+        if self.message:
+            self.message.redraw(window)
         self.x = 0
         self.y = 0
+
+    def collision_action(self):
+        self.message = DialogBox("Ouch!", self)
+
 
 
 class SophieGame():
@@ -87,16 +118,20 @@ class SophieGame():
         self.window_edge = self.window.get_rect()
         self.cat = Cat(50, 50)
         self.cat2 = Cat(100,100)
-
-        self.cats = [self.cat, self.cat2]
+        self.sprites = [self.cat, self.cat2]
 
     def check_window_bounds(self, sprite):
         sprite.rect.clamp_ip(self.window_edge)
 
+    def detect_collision(self, sprite):
+        if self.cat.rect.colliderect(sprite.rect):
+            sprite.collision_action()
+            self.cat.undo_move()
+
     def redraw_game_window(self):
         self.window.fill(BLACK)
-        for cat in self.cats:
-            cat.redraw_cat(self.window)
+        for sprite in self.sprites:
+            sprite.redraw(self.window)
         pygame.display.update()
 
     def main_loop(self):
@@ -118,8 +153,11 @@ class SophieGame():
             if keys[pygame.K_d]: 
                 self.cat.go_right()
 
-            for cat in self.cats:
-                self.check_window_bounds(cat)
+            for sprite in self.sprites:
+                self.check_window_bounds(sprite)
+                if sprite is not self.cat:
+                    self.detect_collision(sprite)
+
             self.redraw_game_window()
 
         pygame.quit()
